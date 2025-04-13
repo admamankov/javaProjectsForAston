c8d617f2f969446da5fe7cdb76d0a13a77abeab5
kchauu6ff <kchauuff@gmail.com>
2025-04-13 20:45:36 +0300

    Update Whale.java

--- firstModule/firstModuleFirstTask/Whale.java
+++ firstModule/firstModuleFirstTask/Whale.java
@@ -1,6 +1,6 @@
 package firstModuleFirstTask;
 
-public class Whale extends Mammal implements CanBreatheUnderwater {
+public class Whale extends Mammal implements LiveInWater {
 
     Whale(String name, int age, double weight, int legsCount, Spine spine) {
         super(name, age, weight, legsCount, spine);
